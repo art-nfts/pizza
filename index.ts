2151ea9f0f1cba31bcaf84a6302220d95cc4ee74
florianthalmann <florianthalmann@bluewin.ch>
2021-06-02 00:29:34 +0900

    story

--- index.ts
+++ index.ts
@@ -1,7 +1,27 @@
 import * as _ from 'lodash';
 
+import dinnerPizzaURL from './images/dinnerpizza2.png';
+import dinnerURL from './images/dinner.jpg';
+import homesURL from './images/homes.jpg';
+import jvilleURL from './images/jacksonsville.jpg';
+import floridaURL from './images/florida.jpg';
 import pizzaURL from './images/pizza.png';
 import earthURL from './images/earth.png';
+
+const dinnerPizza = new Image();
+dinnerPizza.src = dinnerPizzaURL;
+const dinner = new Image();
+dinner.src = dinnerURL;
+const homes = new Image();
+homes.src = homesURL;
+const jville = new Image();
+jville.src = jvilleURL;
+const florida = new Image();
+florida.src = floridaURL;
+const spacePizza = new Image();
+spacePizza.src = pizzaURL;
+const earth = new Image();
+earth.src = earthURL;
 
 const API_KEY = 'DDy7uCcyEjyzzqisk9A5';
 const API_URL = 'https://www.quandl.com/api/v3/datasets/BCHAIN/MKPRU.json?api_key='+API_KEY;
@@ -9,13 +29,7 @@ const API_URL = 'https://www.quandl.com/api/v3/datasets/BCHAIN/MKPRU.json?api_ke
 let context: CanvasRenderingContext2D;
 let data: [string, number][];
 
-const pizza = new Image();
-pizza.src = pizzaURL;
-const earth = new Image();
-earth.src = earthURL;
-
-window.onload = async () => {
-  data = (await (await fetch(API_URL)).json()).dataset.data;
+window.onload = async () => {data = (await (await fetch(API_URL)).json()).dataset.data;
   console.log(data)
   const canvas = <HTMLCanvasElement>document.getElementById('canvas');
   canvas.width = window.innerWidth;
@@ -23,34 +37,51 @@ window.onload = async () => {
   context = canvas.getContext('2d');
   context.font = '10px Arial';
   canvas.innerHTML = "";
-  animateFrom(new Date('2016-01-01'));
+  animateFrom(new Date('2010-07-01'));
 }
 
 async function animateFrom(startDate: Date) {
   const today = new Date();
   const currentDate = startDate;
   while (currentDate <= today) {
-    update(currentDate);
+    if (currentDate < new Date('2010-10-01')) {
+      new Scene(dinnerPizza, [0.5, 0.8], dinner, 5).update(currentDate);
+    } else if (currentDate < new Date('2011-03-01')) {
+      new Scene(dinnerPizza, [0.5, 0.8], homes, 200).update(currentDate);
+    } else if (currentDate < new Date('2013-01-01')) {
+      new Scene(dinnerPizza, [0.5, 0.2], jville, 4000).update(currentDate);
+    } else if (currentDate < new Date('2015-10-01')) {
+      new Scene(dinnerPizza, [0.7, 0.2], florida, 500000).update(currentDate);
+    } else {
+      new Scene(dinnerPizza, [0.668, 0.597], earth, 12742000).update(currentDate);
+    }
     currentDate.setDate(currentDate.getDate()+1);
-    await new Promise(resolve => setTimeout(resolve, 30));
+    await new Promise(resolve => setTimeout(resolve, 10));
   }
 }
 
-async function update(date: Date) {
-  const dateString = date.toISOString().split('T')[0];
-  const price = data.filter(d => d[0] == dateString)[0][1];
-  const earthPos = (window.innerWidth-window.innerHeight)/2;
-  const earthSize = window.innerHeight;
-  const pizzaPrice = 10000*price;
-  const sizeRatio = pizzaPrice/41*0.5/12742000;//50cm pizza
-  const pizzaSize = earthSize*sizeRatio;
-  const pizzaX = earthPos+(earthSize*0.668)-(pizzaSize/2);
-  const pizzaY = (earthSize*0.597)-(pizzaSize/2);
-  context.clearRect(0, 0, window.innerWidth, window.innerHeight);
-  context.drawImage(earth, earthPos, 0, earthSize, earthSize);
-  context.drawImage(pizza, pizzaX+(pizzaSize/50)**2.5, pizzaY-(pizzaSize/80)**2.7, pizzaSize, pizzaSize);
-  context.drawImage(pizza, pizzaX-(pizzaSize/50)**2.5, pizzaY+(pizzaSize/80)**2.6, pizzaSize, pizzaSize);
-  context.font = '48px serif';
-  context.fillStyle = 'white';
-  context.fillText(dateString+', $'+Math.round(pizzaPrice), 20, window.innerHeight-20);
+class Scene {
+  constructor(private pizzaImage: HTMLImageElement,
+      private pizzaRelPos: [number, number],
+      private bgImage: HTMLImageElement, private bgRefSize: number) {
+  }
+  
+  async update(date: Date) {
+    const dateString = date.toISOString().split('T')[0];
+    const price = data.filter(d => d[0] == dateString)[0][1];
+    const bgPos = (window.innerWidth-window.innerHeight)/2;
+    const bgSize = window.innerHeight;
+    const pizzaPrice = 10000*price;
+    const sizeRatio = pizzaPrice/41*1/this.bgRefSize;//2*50cm pizza
+    const pizzaSize = bgSize*sizeRatio;
+    const pizzaX = bgPos+(bgSize*this.pizzaRelPos[0])-(pizzaSize/2);
+    const pizzaY = (bgSize*this.pizzaRelPos[1])-(pizzaSize/2/2.5);
+    context.clearRect(0, 0, window.innerWidth, window.innerHeight);
+    console.log(bgPos, bgSize, pizzaSize)
+    context.drawImage(this.bgImage, bgPos, 0, bgSize, bgSize);
+    context.drawImage(this.pizzaImage, pizzaX, pizzaY, pizzaSize, pizzaSize/2.5);
+    context.font = '48px serif';
+    context.fillStyle = 'white';
+    context.fillText(dateString+', $'+Math.round(pizzaPrice), 20, window.innerHeight-20);
+  }
 }
